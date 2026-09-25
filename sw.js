b2c2782551aa9c4cd77e66d57c31680809ae92c5
Ade Sri Rahayu <adesrirahayu@example.com>
2026-09-25 19:28:27 +0700

    Deploy to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    }).then(() => {
+      return self.registration.unregister();
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
